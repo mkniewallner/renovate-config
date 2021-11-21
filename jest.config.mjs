785485955a11f6adfcec20303fbdb714072bd11b
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2021-11-21 20:24:35 +0100

    Add tests (#8)
    * chore: add tests for regex managers
    
    * chore: add CI for tests

---
+++ jest.config.mjs
@@ -0,0 +1,8 @@
+/*
+ * For a detailed explanation regarding each configuration property, visit:
+ * https://jestjs.io/docs/configuration
+ */
+
+export default {
+  moduleFileExtensions: ["js", "mjs", "node"],
+};
