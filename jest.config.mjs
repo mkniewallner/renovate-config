20dfb14a2c36322c13aad4fcaf41c8dde661cd67
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2021-11-21 22:20:31 +0100

    refactor: remove unneeded `jest` config file

--- jest.config.mjs
+++
@@ -1,8 +0,0 @@
-/*
- * For a detailed explanation regarding each configuration property, visit:
- * https://jestjs.io/docs/configuration
- */
-
-export default {
-  moduleFileExtensions: ["js", "mjs", "node"],
-};
