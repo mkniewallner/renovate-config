4d9d5d33583f6940841ff1b10d670bc5f9e8c5c0
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-09-26 01:25:45 +0200

    chore: use TypeScript (#44)

--- tests/utils.ts
+++ tests/utils.ts
@@ -1,10 +1,10 @@
 import { readFileSync } from "fs";
-import JSON5 from "json5";
+import * as JSON5 from "json5";
 
 export function loadRenovateConfiguration(filePath = "default.json5") {
   return JSON5.parse(readFileSync(filePath, "utf8"));
 }
 
-export function loadFixture(filePath) {
+export function loadFixture(filePath: string) {
   return readFileSync(`tests/__fixtures__/${filePath}`, "utf8");
 }
