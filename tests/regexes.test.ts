13ef46c725c5f3872d31ec533cdc86d3c96ed6c2
Mathieu Kniewallner <mathieu.kniewallner@gmail.com>
2022-10-01 01:17:43 +0200

    Test `fileMatch` in regex managers (#45)
    * test: test `fileMatch` for regex managers
    
    * fix: more specific regex for PEP 440

--- tests/regexes.test.ts
+++ tests/regexes.test.ts
@@ -1,38 +1,62 @@
 import { extractPackageFile } from "renovate/dist/modules/manager/regex";
+import { regEx } from "renovate/dist/util/regex";
 import { loadFixture, loadRenovateConfiguration } from "./utils";
 
-it("should find YAML dependencies", () => {
-  const fileName = "ci.yml";
+function regexMatches(target: string, patterns: string[]): boolean {
+  return patterns.some((pattern: string) => {
+    return regEx(pattern).test(target);
+  });
+}
 
-  const res = extractPackageFile(
-    loadFixture(fileName),
-    fileName,
-    loadRenovateConfiguration()["regexManagers"][0]
-  );
+describe("Update YAML files", () => {
+  const regexManager = loadRenovateConfiguration()["regexManagers"][0];
 
-  expect(res).toMatchSnapshot();
+  it("find dependencies in `ci.yml`", () => {
+    const fileName = "ci.yml";
+    const res = extractPackageFile(loadFixture(fileName), fileName, regexManager);
+
+    expect(res).toMatchSnapshot();
+  });
+
+  describe("matches regexes patterns", () => {
+    test.each([
+      ["ci.yml", true],
+      ["ci.yaml", true],
+      ["ci.yaaml", false],
+      ["ciyml", false],
+      ["ciyaml", false],
+      ["ciayml", false],
+      ["foo/ci.yaml", true],
+      ["foo/bar/ci.yaml", true],
+      ["foo/bar/foo_bar_3000.yaml", true],
+    ])('regexMatches("%s") === %s', (path, expected) => {
+      expect(regexMatches(path, regexManager.fileMatch)).toBe(expected);
+    });
+  });
 });
 
-it("should find pre-commit dependencies", () => {
-  const fileName = ".pre-commit-config.yaml";
+describe("Update PEP 440 Python dependencies", () => {
+  const regexManager = loadRenovateConfiguration()["regexManagers"][1];
 
-  const res = extractPackageFile(
-    loadFixture(fileName),
-    fileName,
-    loadRenovateConfiguration()["regexManagers"][1]
+  test.each([".pre-commit-config.yaml", "pyproject.toml"])(
+    "find dependencies in `%s`",
+    (fileName) => {
+      expect(extractPackageFile(loadFixture(fileName), fileName, regexManager)).toMatchSnapshot();
+    }
   );
 
-  expect(res).toMatchSnapshot();
-});
-
-it("should find dependencies in pyproject.toml", () => {
-  const fileName = "pyproject.toml";
-
-  const res = extractPackageFile(
-    loadFixture(fileName),
-    fileName,
-    loadRenovateConfiguration()["regexManagers"][1]
-  );
-
-  expect(res).toMatchSnapshot();
+  describe("matches regexes patterns", () => {
+    test.each([
+      [".pre-commit-config.yaml", true],
+      ["apre-commit-configayaml", false],
+      [".pre-commit-config.yamlfoobar", false],
+      ["foo/.pre-commit-config.yaml", false],
+      ["pyproject.toml", true],
+      ["pyprojectatoml", false],
+      ["pyproject.tomlfoobar", false],
+      ["foo/pyproject.toml", false],
+    ])('regexMatches("%s") === %s', (path, expected) => {
+      expect(regexMatches(path, regexManager.fileMatch)).toBe(expected);
+    });
+  });
 });
